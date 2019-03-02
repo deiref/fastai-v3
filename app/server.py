462454c69a3129e080872f5806e927c1fd619477
Dan Eiref <dan.eiref@gmail.com>
2019-03-02 17:43:56 -0500

    fix syntax error

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 #export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export file url = 'https://dl.dropboxusercontent.com/s/vy578e5ja8shi56/export.pkl?dl=0'
+export_file_url = 'https://dl.dropboxusercontent.com/s/vy578e5ja8shi56/export.pkl?dl=0'
 export_file_name = 'export.pkl'
 
 #classes = ['black', 'grizzly', 'teddys']
