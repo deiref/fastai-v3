7fbb8d443cb19d9f46c1e9d3ba9f2e528e044d7b
Dan Eiref <dan.eiref@gmail.com>
2019-03-03 15:16:26 -0500

    updated file url

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from fastai.vision import *
 
 #export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 #export_file_url = 'https://dl.dropboxusercontent.com/s/vy578e5ja8shi56/export.pkl?dl=0'
-export_file_url = 'https://dl.dropboxusercontent.com/s/s4xcboxg76bhi38/export.pkl?dl=0
+export_file_url = 'https://dl.dropboxusercontent.com/s/s4xcboxg76bhi38/export.pkl?dl=0'
 export_file_name = 'export.pkl'
 
 #classes = ['black', 'grizzly', 'teddys']
