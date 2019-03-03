52968ab9461bb9f76baa9a6666ffe5901838b3ee
Dan Eiref <dan.eiref@gmail.com>
2019-03-03 17:40:15 -0500

    add image

--- app/server.py
+++ app/server.py
@@ -9,7 +9,6 @@ from fastai import *
 from fastai.vision import *
 
 #export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-#export_file_url = 'https://dl.dropboxusercontent.com/s/vy578e5ja8shi56/export.pkl?dl=0'
 export_file_url = 'https://dl.dropboxusercontent.com/s/s4xcboxg76bhi38/export.pkl?dl=0'
 export_file_name = 'export.pkl'
 
